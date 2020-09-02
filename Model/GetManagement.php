dcb23a17c9f5850d6eae9c82755b5d7144790970
jesus romero <jarrromero@gmail.com>
2020-09-02 13:56:24 -0500

    get best selling products

--- Model/GetManagement.php
+++ Model/GetManagement.php
@@ -15,17 +15,21 @@ namespace Grability\Mobu\Model;
  */
 class GetManagement {
 
-    private $attributeOptions = [];
+    private $response = [];
     private $typesConfigurations = [];
     private $exception;
     private $productRepository;
+    private $collectionFactory;
 
     public function __construct(
         \Magento\Framework\Webapi\Exception $exception,
-        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository
+        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
+        \Magento\Sales\Model\ResourceModel\Report\Bestsellers\CollectionFactory $collectionFactory
     ) {
         $this->exception = $exception;
-        $this->productRepository = $productRepository;    }
+        $this->productRepository = $productRepository;
+        $this->collectionFactory = $collectionFactory;
+    }
 
     public function getProductConfigurations($sku)
     {
@@ -46,8 +50,25 @@ class GetManagement {
                 }
             }
 
-            return $this->attributeOptions;
+            return $this->response;
 
+        } catch(\Exception $e) {
+            throw new $this->exception(__($e->getMessage()),0,$this->exception::HTTP_BAD_REQUEST);
+        }
+    }
+
+    public function getBestSellingProducts($period = 'day')
+    {
+        try {
+            $bestSellers = $this->collectionFactory->create();
+            $bestSellers->setPeriod($period)->getSelect()->order('period DESC')->order('qty_ordered DESC');
+
+            foreach ($bestSellers as $key => $bestSeller) {
+                $this->response[$key]['period'] = $bestSeller->getdata('period');
+                $this->response[$key]['sku'] = $this->productRepository->getById($bestSeller->getProductId())->getSku();
+            }
+
+            return $this->response;
         } catch(\Exception $e) {
             throw new $this->exception(__($e->getMessage()),0,$this->exception::HTTP_BAD_REQUEST);
         }
@@ -65,7 +86,7 @@ class GetManagement {
             $this->typesConfigurations[] = strtolower($productAttribute['label']);
 
             foreach ($productAttribute['values'] as $attribute) {
-                $this->attributeOptions[$mapProcessIndex]['configurations'][strtolower($productAttribute['label'])][] = [
+                $this->response[$mapProcessIndex]['configurations'][strtolower($productAttribute['label'])][] = [
                     'index' => $attribute['value_index'],
                     'value' => $attribute['store_label']
                 ];
@@ -76,14 +97,14 @@ class GetManagement {
     protected function mapMultiReference($mapProcessIndex, $productsChildren)
     {
         foreach ($productsChildren as $key => $child) {
-            $this->attributeOptions[$mapProcessIndex]['multiReference'][$key] = [
+            $this->response[$mapProcessIndex]['multiReference'][$key] = [
                 'id' => $child->getId(),
                 'sku' => $child->getSku()
             ];
 
             foreach ($this->typesConfigurations as $type) {
                 if ($child->getCustomAttribute($type) !== null) {
-                    $this->attributeOptions[$mapProcessIndex]['multiReference'][$key][$type] = $child->getCustomAttribute($type)->getValue();
+                    $this->response[$mapProcessIndex]['multiReference'][$key][$type] = $child->getCustomAttribute($type)->getValue();
                 }
             }
         }
