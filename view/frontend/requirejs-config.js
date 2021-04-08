c86c305e65f936161848bdec42b7adbbd0dab932
DanielCardenasGraber <80707938+DanielCardenasGraber@users.noreply.github.com>
2021-04-08 15:27:57 -0500

    M20-190 bug en grilla con productos configurables (#18)
    * M20-190 bug en grilla con productos configurables 
    
    se sobrescribe y edita archivo swatch-renderer.js de vendor
    
    * archivo configuración para sobreescritura de js
    
    archivo configuración para sobre escritura de js

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Swatches/js/swatch-renderer': {
+                'Grability_Mobu/js/swatch-renderer-mixin': true
+            }
+        }
+    }
+};
